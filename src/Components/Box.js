daa7244402070444787684bc7eb52cfb34880118
Promise <tshegofatsopromise362@gmail.com>
2023-10-26 12:35:59 -0700

    MUi practise

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -4,12 +4,10 @@ import React from 'react'
 function BoxLayOut() {
     return (
         // box acts as a div
-        <Box sx={{backgroundColor:'burlywood', height:"100vh", width: "100%"}}>
-            <Button variant='contained' sx={{backgroundColor:'chocolate'}}>First</Button>
-            <Button variant='contained' sx={{backgroundColor:'chocolate'}}>Second</Button>
-            <Button variant='contained' sx={{backgroundColor:'chocolate'}}>Third</Button>
-            
-
+        <Box  sx={{backgroundColor:'burlywood', height:"100vh", width: "100%",display:"flex",flexDirection:"column", rowGap:"20px", justifyContent: "center",alignItems:"center"}} >
+            <Button variant='contained' sx={{backgroundColor:'chocolate',width:'300px'}}>First</Button>
+            <Button variant='contained' sx={{backgroundColor:'chocolate', width:'300px'}}>Second</Button>
+            <Button variant='contained' sx={{backgroundColor:'chocolate', width:'300px'}}>Third</Button>
         </Box>
     )
 }
